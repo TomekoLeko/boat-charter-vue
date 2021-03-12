277f0d59b56a1cda7a2ae5d8ac1d8eb503a626cd
TomekoLeko <ays@vp.pl>
2021-03-12 12:47:23 +0100

    feat: navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="#03A9F4"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Boat Charter"
-          class="shrink mr-2"
-          contain
-          src="./assets/logo2.png"
-          transition="scale-transition"
-          width="80"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
+    <nav-bar />
     <v-main>
     <carousel />  
     </v-main>
@@ -27,12 +9,15 @@
 
 <script>
 import Carousel from './components/Carousel';
+import NavBar from './components/NavBar';
 
 export default {
   name: 'App',
 
   components: {
-    Carousel
+    Carousel,
+    NavBar,
+
   },
 
   data: () => ({
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-app-bar
+      app
+      color="#03A9F4"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Boat Charter"
+          class="shrink mr-2"
+          contain
+          src="../assets/logo2.png"
+          transition="scale-transition"
+          width="80"
+        />
+      </div>
+      <v-tabs align-with-title>
+          <v-tab>LINK 1</v-tab>
+          <v-tab>LINK 2</v-tab>
+          <v-tab>LINK 3</v-tab>
+        </v-tabs>
+
+    </v-app-bar>
+</template>
+
+
+<script>
+  export default {
+    name: 'NavBar',
+ data () {
+      return {
+     
+      }
+    },
+  }
+</script>
+
+<style scoped>
+.link:hover {
+    color:rgba(220,220,220);
+
+}
+
+</style>
