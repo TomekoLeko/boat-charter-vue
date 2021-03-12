6014cb60838f8f3a064ef74d5ca89c07d6daf90e
TomekoLeko <ays@vp.pl>
2021-03-12 11:59:02 +0100

    feat: first verions of navbar, logo and main carousel

--- src/App.vue
+++ src/App.vue
@@ -2,59 +2,37 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="#03A9F4"
       dark
     >
-    <v-avatar
-  color="primary"
-  size="56"
-></v-avatar>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Boat Charter"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/logo2.png"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          width="80"
         />
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+    <carousel />  
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Carousel from './components/Carousel';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Carousel
   },
 
   data: () => ({
@@ -62,3 +40,6 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,61 @@
+    <template>
+  <v-carousel
+    cycle
+    height="70vh"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      class="carousel"
+    >
+    <div class="slide-text">{{item.text}}</div>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    name: 'Carousel',
+ data () {
+      return {
+                  items: [
+          {
+            src: require("@/assets/carousel-1.jpg"),
+            text: 'Slide nr 1',
+          },
+          {
+            src: require("@/assets/carousel-2.jpg"),
+            text: 'Slide nr 2'
+          },
+        ],
+        colors: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+      }
+    },
+  }
+</script>
+
+<style scoped>
+.carousel {
+    position: relative;
+}
+.slide-text {
+ width:auto;
+ height:auto;
+ color: rgba(255, 255, 255, 0.639);
+ font-size:10vw;
+ text-shadow: 4px 4px 7px rgb(97, 114, 121);
+ position: absolute;
+ right:0;
+ bottom:0;
+}
+
+</style>
